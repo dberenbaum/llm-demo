524058e7cdec06bfb3662dc467731ad0718862ac
dberenbaum <dave@iterative.ai>
2024-01-08 15:27:49 -0500

    switch embeddings to ollama

--- vector_store.py
+++ vector_store.py
@@ -1,12 +1,12 @@
 import faiss
 from langchain.vectorstores import FAISS
-from langchain.embeddings import OpenAIEmbeddings
+from langchain.embeddings import OllamaEmbeddings
 import pickle
 import json
 import dvc.api
 
 
-params = dvc.api.params_show()['OpenAIEmbeddings']
+params = dvc.api.params_show()['Embeddings']
 print(params)
 
 with open("docs.json", "r") as f:
@@ -17,14 +17,10 @@ with open("metadatas.json", "r") as f:
 
 print(f"Processing {len(docs)} documents.")
 
-emb = OpenAIEmbeddings(chunk_size=params['chunk_size'],
-                       embedding_ctx_length=params['embedding_ctx_length'],
-                       max_retries=params['max_retries'],
-                       model=params['model'])
-emb.show_progress_bar = True
+emb = OllamaEmbeddings(**params)
 
 # Here we create a vector store from the documents and save it to disk.
-store = FAISS.from_texts(docs, emb, metadatas=metadatas)
+store = FAISS.from_texts(docs[:100], emb, metadatas=metadatas[:100])
 faiss.write_index(store.index, "docs.index")
 store.index = None
 with open("faiss_store.pkl", "wb") as f:
