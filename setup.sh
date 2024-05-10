5f2b136e632f8e4b20c38e7b1db97ab4fa59c4f6
Dave Berenbaum <dave@iterative.ai>
2024-05-10 14:31:35 -0400

    don't prompt in setup script

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 set -e
 
-conda create -n mlops-for-llms-workshop python==3.11
+conda create -y -n mlops-for-llms-workshop python==3.11
 conda activate mlops-for-llms-workshop
 pip install --no-input -r requirements.txt
