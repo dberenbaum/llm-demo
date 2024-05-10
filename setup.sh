9c1fa9bbda280800f24a14ce7b85d775771c7354
Dave Berenbaum <dave@iterative.ai>
2024-05-10 14:52:36 -0400

    debug setup

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 set -e
 
 conda create -y -n mlops-for-llms-workshop python==3.11
+conda init
+source /home/vscode/.bashrc
 conda activate mlops-for-llms-workshop
 pip install --no-input -r requirements.txt
